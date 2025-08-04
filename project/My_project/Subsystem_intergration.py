1add4bd494407f9276dfad569686af5b0a16414c
sphisi <143594972+sphisi@users.noreply.github.com>
2025-08-04 11:55:32 +1000

    v0.0.2

--- project/My_project/Subsystem_intergration.py
+++ project/My_project/Subsystem_intergration.py
@@ -1,1 +1,13 @@
-#combine diffirent movement states
+#combine diffirent movement states to creat autonomous
+
+#import libaries
+
+#define left motor
+left_motor = 
+#define right motor
+right_motor = 
+
+#define forward facing ultrasonic sensor
+
+#define right facing ultrasonic sensor
+
