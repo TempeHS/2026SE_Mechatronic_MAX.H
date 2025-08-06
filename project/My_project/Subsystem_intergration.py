5a9a92786a40fddfd23ab14fb92ab83506c94a21
sphisi <143594972+sphisi@users.noreply.github.com>
2025-08-06 15:05:07 +1000

    0.0.2

--- project/My_project/Subsystem_intergration.py
+++ project/My_project/Subsystem_intergration.py
@@ -1,11 +1,17 @@
 #combine diffirent movement states to creat autonomous
 
 #import libaries
+from basicmovement import Movementsubsystem
+from servo import Servo
+import time
+from 
+
 
 #define left motor
-left_motor = 
+left_motor = Movementsubsystem()
+
 #define right motor
-right_motor = 
+right_motor = Movementsubsystem()
 
 #define forward facing ultrasonic sensor
 
---
+++ project/My_project/controller.py
@@ -0,0 +1,9 @@
+import time 
+import PiicoDev_Ultrasonic
+from basicmovement import Movementsubsystem
+#combine basic movement and ultrasonic sensor to creat advanced path finding
+
+
+class Turn():
+
+class PID_forward():
