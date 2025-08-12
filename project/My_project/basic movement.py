de649513b716f2c14309149f366a28f41f5d7177
sphisi <143594972+sphisi@users.noreply.github.com>
2025-08-12 13:20:11 +1000

    0.0.6

--- project/My_project/basic movement.py
+++ project/My_project/basic movement.py
@@ -6,68 +6,111 @@ from PiicoDev_Unified import sleep_ms
 
 class Basic_movement(Servo):
     def __init__(self, Right_servo, Left_servo):
-        self.Left_servo = Servo(pwm=servo_pwm, min_us=500, max_us=2500, dead_zone_us=1500, freq=50)
-        self.Right_servo = Servo(pwm=servo_pwm, min_us=500, max_us=2500, dead_zone_us=1500, freq=50)
+        self.__Left_servo = Servo(pwm=20, min_us=500, max_us=2500, dead_zone_us=1500, freq=50)
+        self.__Right_servo = Servo(pwm=16, min_us=500, max_us=2500, dead_zone_us=1500, freq=50)
 
     def Stop(self):
-        self.Right_servo.set_duty(1500)
-        self.Left_servo.set_duty(1500)
+        self.__Right_servo.set_duty(1500)
+        self.__Left_servo.set_duty(1500)
 
     def Turn_right(self):
-        self.Right_servo.set_duty(2000)
-        self.Left_servo.set_duty(1500)
+        self.__Right_servo.set_duty(2000)
+        self.__Left_servo.set_duty(1500)
 
     def Turn_left(self):
-        self.Right_servo.set_duty(1500)
-        self.Left_servo.set_duty(2000)
+        self.__Right_servo.set_duty(1500)
+        self.__Left_servo.set_duty(2000)
 
     #use with caution
     def Basic_backward(self):
-        self.Right_servo.set_duty(1000)
-        self.Left_servo.set_duty(1000)
+        self.__Right_servo.set_duty(1000)
+        self.__Left_servo.set_duty(1000)
 
     def basic_forward(self):
-        self.Right_servo.set_duty(2000)
-        self.Left_servo.set_duty(2000)
+        self.__Right_servo.set_duty(2000)
+        self.__Left_servo.set_duty(2000)
 
-class Ultra_sensor_states(PiicoDev_Ultrasonic, Basic_movement):
+    def right_forward(self):
+        self.__Right_servo.set_duty(1800)
+        self.__Left_servo.set_duty(2000)
+
+    def left_forward(self):
+        self.__Right_servo.set_duty(2000)
+        self.__Left_servo.set_duty(1800)
+
+
+class Ultra_sensor_states(PiicoDev_Ultrasonic, range_a, range_b):
     def __init__(self, range_a, range_b)
-        self.range_a = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-        self.range_b = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+        self.__range_a = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
+        self.__range_b = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
 
     def check_forward():
-        return self.range_a.distance_mm
+        return self.__range_a.distance_mm
 
     def check_right():
-        return self.range_b.distance_mm 
-
+        return self.__range_b.distance_mm 
     
 
-class Combined_movement(Basic_movement, Ultra_sensor_states):
-    #checkl controller for states
-    def __init__(self, ):
-        self.state = "IDLE"
-    #check controller
-    def set_state(self):
+class Check_colour():
 
-    def Idle():
+
+class Combined_movement(Basic_movement, Ultra_sensor_states, sleep_ms):
+    #checkl controller for states
+    def __init__(self, last_forward_distance, last_right_distance, state):
+        self.state = "idle"
+        self.last_forward_distance = Ultra_sensor_states.check_forward
+        self.last_right_distance = Ultra_sensor_states.check_right
+
+    def set_Idle():
         print("IDLE")
         Basic_movement.Stop
-    def Forward():
-        last_forward_distance = Ultra_sensor_states.check_forward
-        last_right_distance = Ultra_sensor_states.check_right
-        print("FORWARD")
-        sleep_ms(5)
-        if Ultra_sensor_states.check_forward < 20:
-            if Ultra_sensor_states.check_right <20:
-        else:
-            self.state = self.set_state_forward
+        self.state = "idle"
 
-    def Right():
+    def set_Forward():
+        print("FORWARD")
+        Basic_movement.basic_forward
+        self.state = "forward"
+            
+    def set_Right():
         print("RIGHT")
-    def Left():
+        Basic_movement.Turn_right
+        self.state = "right"
+
+    def set_Left():
         print("LEFT")
+        Basic_movement.Turn_left
+        self.state = "left"
 
     def Run(self):
+        self.state = self.set_Idle
+        print("running in 3 seconds")
+        sleep_ms(30)
         print("RUNNING")
-        if 
+        if self.last_forward_distance < 100:
+            if self.last_right_distance <100:
+                self.state = self.set_Left
+            else:
+                self.state = self.set_Right
+        else:
+            self.state = self.set_Forward
+
+class debug(self, Ultra_sensor_states, Check_colour, Basic_movement)
+    def __init__(self, state)
+        self.state = state
+
+    def debug_ultra_sensor():
+        #print range for both sensors in sequence 
+        while True
+            print("forward")
+            print(Ultra_sensor_states.check_forward)
+            sleep_ms(5)
+            print("right")
+            print(Ultra_sensor_states.check_right)
+            sleep_ms(5)
+    
+    def debug_colour():
+        while True
+        #check colour, red green blue in sequence while broadcasting
+
+    def debug_movement():
+        #
