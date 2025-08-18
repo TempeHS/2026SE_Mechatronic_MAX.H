7fa965ff40ab038248caa267a0fcacfec8200738
sphisi <143594972+sphisi@users.noreply.github.com>
2025-08-18 11:55:20 +1000

    0.1.2

--- project/py_scripts/omega.py
+++ project/py_scripts/omega.py
@@ -1,15 +1,13 @@
-import time 
 from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
 from servo import Servo
-from PID_Controller import PIDControl
 from PiicoDev_Unified import sleep_ms
 from PiicoDev_VEML6040  import PiicoDev_VEML6040
 
-
-class Basic_movement(Servo):
-    def __init__(self):
-        self.__Left_servo = Servo(pwm=20)
-        self.__Right_servo = Servo(pwm=16)
+#(Servo)
+class Basic_movement:
+    def __init__(self, Left_servo, Right_servo):
+        self.__Left_servo = Left_servo
+        self.__Right_servo = Right_servo
 
     def Stop(self):
         self.__Right_servo.set_duty(1500)
@@ -40,11 +38,11 @@ class Basic_movement(Servo):
         self.__Right_servo.set_duty(2000)
         self.__Left_servo.set_duty(1800)
 
-
-class Ultra_sensor_states(PiicoDev_Ultrasonic):
+#(PiicoDev_Ultrasonic)
+class Ultra_sensor_states:
     def __init__(self, range_a, range_b):
-        self.__range_a = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-        self.__range_b = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+        self.__range_a = range_a
+        self.__range_b = range_b
 
     def check_forward(self):
         return int(self.__range_a.distance_mm)
@@ -52,8 +50,8 @@ class Ultra_sensor_states(PiicoDev_Ultrasonic):
     def check_right(self):
         return int(self.__range_b.distance_mm)
     
-
-class Check_colour(PiicoDev_VEML6040):
+#(PiicoDev_VEML6040)
+class Check_colour:
     def __init__(self):
         self.colourSensor = PiicoDev_VEML6040()
         self.green = 120
@@ -64,45 +62,44 @@ class Check_colour(PiicoDev_VEML6040):
         ...
                 
 
-
-class Combined_movement(Basic_movement, Ultra_sensor_states, Servo, PiicoDev_Ultrasonic):
-    def __init__(self):
+#(Basic_movement, Ultra_sensor_states, Servo, PiicoDev_Ultrasonic)
+class Combined_movement:
+    def __init__(self,):
         self.__state = "idle"
-        ultrasonic = Ultra_sensor_states(
+        self.ultrasonic = Ultra_sensor_states(
             range_a=PiicoDev_Ultrasonic(id=[0, 0, 0, 0]),
             range_b=PiicoDev_Ultrasonic(id=[1, 0, 0, 0]) 
             )
-        movement = Basic_movement(
-            Right_servo=Servo(pwm=16,),
-            Left_servo=Servo(pwm=20,)
-            )
-        self.last_forward_distance = int(ultrasonic.check_forward())
-        self.last_right_distance = int(ultrasonic.check_right())
+        self.movement = Basic_movement(Servo(pwm=16), Servo(pwm=20))
+        self.last_forward_distance = int(self.ultrasonic.check_forward())
+        self.last_right_distance = int(self.ultrasonic.check_right())
 
     def set_Idle(self):
         print("IDLE")
-        self.movement.Stop
+        self.movement.Stop()
         self.__state = "idle"
 
     def set_Forward(self):
         print("FORWARD")
-        self.movement.basic_forward
+        self.movement.basic_forward()
         self.__state = "forward"
             
     def set_Right(self):
         print("RIGHT")
-        self.movement.Turn_right
+        self.movement.Turn_right()
         self.__state = "right"
 
     def set_Left(self):
         print("LEFT")
-        self.movement.Turn_left
+        self.movement.Turn_left()
         self.__state = "left"
 
     def Run(self):
         print("RUNNING")
-        if self.last_forward_distance < 100:
-            if self.last_right_distance < 100:
+        forward_distance = self.ultrasonic.check_forward()
+        right_distance = self.ultrasonic.check_right()
+        if forward_distance < 100:
+            if right_distance < 100:
                 self.set_Left()
             else:
                 self.set_Right()
@@ -115,13 +112,14 @@ class debug(Ultra_sensor_states, Check_colour, Basic_movement):
         
 
     def debug_ultra_sensor(self):
-        #print range for both sensors in sequence 
+        sensor = Ultra_sensor_states(
+        range_a=PiicoDev_Ultrasonic(id=[0, 0, 0, 0]),
+        range_b=PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+        )
         while True:
-            print("forward")
-            print(Ultra_sensor_states.check_forward)
+            print("forward:", sensor.check_forward())
             sleep_ms(5)
-            print("right")
-            print(Ultra_sensor_states.check_right)
+            print("right:", sensor.check_right())
             sleep_ms(5)
     
     def debug_colour(self):
